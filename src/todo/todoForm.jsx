b04caf1096c0b6588a0240686bd9c9bb1780c095
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-24 14:51:16 -0300

    feat: botão e renderizacao condicional

---
+++ src/todo/todoForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Grid from "../template/grid";
+import IconButton from "../template/iconButton";
+
+export default props =>(
+    <div role="form" className="todoForm">
+        <Grid cols="12 9 10">
+            <input type="text" className="form-control" id="description" placeholder="Adicione uma tarefa"/>
+        </Grid>
+            
+
+        <Grid cols="12 3 2">
+            <IconButton style="primary" icon = "plus">
+                <i className="fa fa-plus"></i>
+            </IconButton>
+         
+        </Grid>
+    </div>
+)
